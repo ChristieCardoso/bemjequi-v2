b89c6fce8548b97d58a70f26b0c4b2438af40514
ChristieCardoso <christiecardoso@hotmail.com>
2023-12-05 13:55:50 -0300

    edit about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,31 +1,25 @@
 import "./About.scss";
-// import bg from "./g.jpg";
 
 export const About = () => {
   return (
     <section id="about" className="about">
-      <div className="container">
-        <div className="row">
-          <div className="aboutContainer">
-            <div
-              // style={{ backgroundImage: `url(${bg})` }}
-              className="aboutContainerImg bgImg"
-            ></div>
+      <div className="aboutContainer">
+        <div className="aboutContainerImg"></div>
 
-            <div className="aboutContainerDetails">
-              <h1>
-                <span className="colorGrey">We</span> create delicious memories
-                for<span className="colorGrey"> us</span>
-              </h1>
-              <p>
-                We cook the best food in the entire city, with excellent
-                customer service, the best meals and at the best price, visit
-                us.
-              </p>
-            </div>
-          </div>
+        <div className="aboutContainerDetails">
+          <h1>
+            <span className="colorGrey"></span> Bemjequi - A Origem do Sabor Autêntico<span className="colorGrey"> </span>
+          </h1>
+          <p>
+            No Vale jequitionha nasceu a marca Bemjequi, sinônimo de excelência em queijo artesanal. Há mais de dois anos, nossa família se dedica à tradição queijeira. Nossos queijos são meticulosamente produzidos, utilizando apenas os melhores ingredientes locais e um cuidado especial em cada etapa do processo.
+          </p>
+          <p>
+            Orgulhosos de nossas raízes e da paixão que temos por criar queijos excepcionais, estamos constantemente em busca de inovação sem jamais abrir mão da tradição. Bem-vindo ao universo Bemjequi, onde cada mordida é uma viagem ao coração do interior do Vale Jequitionha.
+          </p>
         </div>
       </div>
+
+
     </section>
   );
 };
